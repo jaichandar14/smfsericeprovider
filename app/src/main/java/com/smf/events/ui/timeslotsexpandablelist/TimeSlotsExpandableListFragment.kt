b78e5f7af8803b54e7a4a2b8f21fb5d0b7fffa29
P Vigneshwaran <vigneshwaran.palanichamy@bpmlinks.com>
2022-05-13 10:18:39 +0530

    Task - 2558(Booked Event Time Slot Api's Integration(New Json Format))
    - Added time_slot_bottom.xml file for expandedList inside last view.
    - Added time_slot_title_collapse.xml file for expandableList title with collapse state view.
    - Inside CustomExpandableListAdapter.kt implemented new data based on api json format.
    - Removed ExpandableListData.kt file.
    - Inside fragment_time_slots_expandable_list.xml changed divider height 10dp to 0dp.
    - ListData.kt file updated based on new api Json.
    - Inside ScheduleManagementViewModel.kt added liveData for current Date.
    - Inside TimeSlotsFragment.kt and TimeSlotsExpandableListFragment.kt implemented sharedViewModel.
    - time_slot_list.xml and time_slot_title.xml changed variable dp based on new api implementation.

--- app/src/main/java/com/smf/events/ui/timeslotsexpandablelist/TimeSlotsExpandableListFragment.kt
+++ app/src/main/java/com/smf/events/ui/timeslotsexpandablelist/TimeSlotsExpandableListFragment.kt
@@ -1,63 +1,77 @@
 package com.smf.events.ui.timeslotsexpandablelist
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ExpandableListView
-import android.widget.ImageView
-import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
-import com.smf.events.BR
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.smf.events.R
-import com.smf.events.base.BaseFragment
-import com.smf.events.databinding.FragmentTimeSlotsExpandableListBinding
+import com.smf.events.ui.schedulemanagement.ScheduleManagementViewModel
 import com.smf.events.ui.timeslotsexpandablelist.adapter.CustomExpandableListAdapter
-import com.smf.events.ui.timeslotsexpandablelist.model.ExpandableListData.data
-import com.smf.events.ui.timeslotsexpandablelist.model.TitleData
-import dagger.android.support.AndroidSupportInjection
-import javax.inject.Inject
+import com.smf.events.ui.timeslotsexpandablelist.model.ListData
+import com.smf.events.ui.timeslotsexpandablelist.model.Status
 
-class TimeSlotsExpandableListFragment :
-    BaseFragment<FragmentTimeSlotsExpandableListBinding, TimeSlotsExpandableListViewModel>(),
+class TimeSlotsExpandableListFragment : Fragment(),
     CustomExpandableListAdapter.TimeSlotIconClickListener {
 
     private var expandableListView: ExpandableListView? = null
     private var adapter: CustomExpandableListAdapter? = null
-    private var titleList: List<TitleData>? = null
+    private var topics = HashMap<String, List<ListData>>()
+    private var langs = ArrayList<String>()
 
-    @Inject
-    lateinit var factory: ViewModelProvider.Factory
+    // SharedViewModel Initialization
+    private val sharedViewModel: ScheduleManagementViewModel by activityViewModels()
 
-    override fun getViewModel(): TimeSlotsExpandableListViewModel =
-        ViewModelProvider(this, factory).get(TimeSlotsExpandableListViewModel::class.java)
-
-    override fun getBindingVariable(): Int = BR.timeSlotsExpandableListViewModel
-
-    override fun getContentView(): Int = R.layout.fragment_time_slots_expandable_list
-
-    override fun onAttach(context: Context) {
-        AndroidSupportInjection.inject(this)
-        super.onAttach(context)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_time_slots_expandable_list, container, false)
+        expandableListView = root.findViewById(R.id.expendableList)
+        return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("TAG", "onViewCreated: TimeSlotsExpandableListFragment called")
-        // 2527 - Initialize expendableList
-        expandableListView = mDataBinding?.expendableList
+        // 2558 - Get ExpandableList Data
+        fillData()
+        // 2558 - ExpandableList Initialization
         initializeExpandableListSetUp()
-
+        // 2558 - getDate ScheduleManagementViewModel Observer
+        sharedViewModel.getDate.observe(requireActivity(), {
+            Log.d("TAG", "onCreateView viewModel called TimeSlotsExpandableListFragment: $it")
+        })
     }
 
+    // 2558 - Method for Get ExpandableList Data
+    private fun fillData() {
+        langs.add("05/09/2022")
+        langs.add("05/19/2022")
+        val java = ArrayList<ListData>()
+
+        val one = ArrayList<Status>()
+        one.add(Status("Catering", "Food Capital", "Vi Vi Bday", "10/10/2022"))
+        one.add(Status("Salon", "Salon Shop", "Vi Vi Bday", "10/10/2022"))
+        one.add(Status("Wedding", "Wedding Shop", "Vi Vi Bday", "10/10/2022"))
+        one.add(Status("Party", "Party Shop", "Vi Vi Bday", "10/10/2022"))
+
+        java.add(ListData("12am - 3am", one))
+        java.add(ListData("3am - 6am", one))
+        topics.put(langs[0], java)
+        topics.put(langs[1], java)
+    }
+
+    // 2558 - Method for ExpandableList Initialization
     private fun initializeExpandableListSetUp() {
         if (expandableListView != null) {
-            val listData = data
-            titleList = ArrayList(listData.keys)
             adapter = CustomExpandableListAdapter(
                 requireContext(),
-                titleList as ArrayList<TitleData>,
-                listData
+                langs,
+                topics
             )
             expandableListView!!.setAdapter(adapter)
             adapter?.setOnClickListener(this)
@@ -71,6 +85,13 @@ class TimeSlotsExpandableListFragment :
 
                 return@setOnChildClickListener false
             }
+
+            expandableListView!!.setOnGroupCollapseListener { groupPosition ->
+                Log.d(
+                    "TAG",
+                    "initializeExpandableListSetUp: clope $groupPosition"
+                )
+            }
         }
     }
 
@@ -79,8 +100,9 @@ class TimeSlotsExpandableListFragment :
         Log.d("TAG", "onViewCreated: TimeSlotsExpandableListFragment onResume called")
     }
 
-    override fun onClick(listPosition: Int) {
-        Log.d("TAG", "actionCardClick: clicked")
+    override fun onClick(expandedListPosition: Int) {
+        Log.d("TAG", "onCreateView viewModel called $expandedListPosition")
+        sharedViewModel.setDate("05/20/2022")
     }
 
 }
--- app/src/main/java/com/smf/events/ui/timeslotsexpandablelist/model/ListData.kt
+++ app/src/main/java/com/smf/events/ui/timeslotsexpandablelist/model/ListData.kt
@@ -1,3 +1,10 @@
 package com.smf.events.ui.timeslotsexpandablelist.model
 
-data class ListData(val timeSlot: String, val status: String)
+data class ListData(val timeSlot: String, val status: List<Status>)
+
+data class Status(
+    val ServiceName: String,
+    val branchName: String,
+    val eventName: String,
+    val eventDate: String
+)
